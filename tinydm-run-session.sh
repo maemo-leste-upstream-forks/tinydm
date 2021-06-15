maemo-leste-upstream-forks/tinydm

commit a7b0eaf3dab200daa21d8c263b05d850b699da40

Add support for sourcing env profiles based on session type (MR 9)
This adds the ability to source any files under
/etc/tinydm.d/$session_type.d, where session_type is either x11 or
wayland. The intention is to allow users to specify variables that
should only be set for specific session types and not others, e.g.
`MOZ_ENABLE_WAYLAND=1` or `SDL_VIDEODRIVER=wayland`.

tinydm-run-session.sh
@@ -22,6 +22,24 @@ source_profile() {
 	done
 }
 
+# $1: session type (i.e. 'wayland', or 'x11')
+source_session_profiles() {
+	session_type="$1"
+	if [ "$session_type" != "wayland" ] && [ "$session_type" != "x11" ]; then
+		echo "Unknown session type: $session_type"
+		exit 1
+	fi
+
+	[ ! -d "/etc/tinydm.d/env-${session_type}.d" ] && return
+
+	for file in "/etc/tinydm.d/env-${session_type}.d/"*; do
+		echo "tinydm: sourcing file: $file"
+		# shellcheck disable=SC1090
+		. "$file"
+	done
+}
+
+
 # $1: file
 # $2: key
 parse_xdg_desktop() {
@@ -72,10 +90,12 @@ run_session() {
 
 	case "$resolved" in
 		/usr/share/wayland-sessions*)
+			source_session_profiles wayland
 			# shellcheck disable=SC2086
 			run_session_wayland $cmd
 			;;
 		/usr/share/xsessions*)
+			source_session_profiles x11
 			# shellcheck disable=SC2086
 			run_session_x $cmd
 			;;

Makefile
@@ -18,3 +18,6 @@ install:
 		install -Dm644 tinydm.confd \
 			"$(DESTDIR)/etc/conf.d/tinydm"; \
 	fi
+
+	mkdir -p $(DESTDIR)/etc/tinydm.d/env-wayland.d
+	mkdir -p $(DESTDIR)/etc/tinydm.d/env-x11.d

commit 2e2b31e0922f344d7b46f74ef8f7d0aa3bfd36a4

tinydm-run-session: fix crash with empty env dir (MR 10)
Fix crash if /etc/tinydm.d/env-wayland.d dir exists (as created by
Makefile) but is empty:

  tinydm: sourcing file: /etc/tinydm.d/env-wayland.d/*
  /usr/bin/tinydm-run-session: .: line 38: can't open '/etc/tinydm.d/env-wayland.d/*': No such file or directory

Fixes: a7b0eaf3 ("Add support for sourcing env profiles based on session type")

tinydm-run-session.sh
@@ -30,9 +30,11 @@ source_session_profiles() {
 		exit 1
 	fi
 
-	[ ! -d "/etc/tinydm.d/env-${session_type}.d" ] && return
-
 	for file in "/etc/tinydm.d/env-${session_type}.d/"*; do
+		if ! [ -e "$file" ]; then
+			continue
+		fi
+
 		echo "tinydm: sourcing file: $file"
 		# shellcheck disable=SC1090
 		. "$file"